40698f0b14a24eee6650b556d5780cce1bae5be5
giild <peter@giild.com>
2024-11-10 20:14:38 -0500

    change input output to increase memory and gpu usage

--- cifar10_train.py
+++ cifar10_train.py
@@ -16,8 +16,8 @@ batchSize = 128
 class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 128, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
         self.pool1 = nn.MaxPool2d(2, 2)
         self.fc1 = nn.Linear(128 * 4 * 4, 256)
